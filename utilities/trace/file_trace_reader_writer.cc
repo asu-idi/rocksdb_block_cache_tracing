fd6f498af69ce7fa20972b7b219ef3f1a96998ab
Qiaolin Yu <liin1211@outlook.com>
2023-02-05 21:41:56 +0800

    try flush frequently

--- utilities/trace/file_trace_reader_writer.cc
+++ utilities/trace/file_trace_reader_writer.cc
@@ -97,7 +97,11 @@ Status FileTraceWriter::Close() {
 }
 
 Status FileTraceWriter::Write(const Slice& data) {
-  return file_writer_->Append(data);
+  Status s = file_writer_->Append(data);
+  if (!s.ok()) {
+    return s;
+  }
+  return file_writer_->Flush();
 }
 
 uint64_t FileTraceWriter::GetFileSize() { return file_writer_->GetFileSize(); }
